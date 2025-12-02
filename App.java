26fe7521ab197facc573979ae237102909792b0b
Deeshna105 <chowdurinds@gmail.com>
2025-12-02 10:47:14 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+package MavenJavaProject_1.mavenjava_1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
